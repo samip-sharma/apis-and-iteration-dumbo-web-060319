e82b90ff0dffb138a53332fe94372d56be0b6b8b
github username <github email address>
2019-06-04 13:42:15 -0400

    another step

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -17,7 +17,6 @@ def get_character_movies_from_api(character_name)
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
 
-
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   
@@ -26,30 +25,28 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-  binding.pry
+
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |film_hash|
+    puts "--"*10
+    puts "Title:"
+    puts film_hash["title"]
+    puts "Description"
+    puts film_hash["opening_crawl"]
+  end
+
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
-
-get_character_movies_from_api("samip")
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
  
 
 def film_api_helper(url)
 response_string=RestClient.get url
 response_hash=JSON.parse(response_string)
-binding.pry
 end
 
-  film_api_helper("https://www.swapi.co/api/films/2/")
-  [{}]
